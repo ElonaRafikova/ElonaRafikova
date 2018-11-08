7467873f3c32f1c94c28681631ba67a58b213400
el.raf <36075706+elonarafikova@users.noreply.github.com>
2018-11-08 12:25:00 +0300

    hw8 dataproviders

--- src/main/java/hw8/dataProviders/MetalAndColorsDataProviders.java
+++ src/main/java/hw8/dataProviders/MetalAndColorsDataProviders.java
@@ -1,27 +1,33 @@
 package hw8.dataProviders;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
 import org.testng.annotations.DataProvider;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MetalAndColorsDataProviders {
-    @DataProvider
-    public static Object[][] metalAndColorsDataProvider() throws FileNotFoundException {
+    private static String filePath = "src\\main\\resources\\JDI_ex8_metalsColorsDataSet.json";
 
-        Gson gson = new Gson();
-        JsonObject jDataSet = new JsonParser().parse(new FileReader
-                ("src\\main\\resources\\JDI_ex8_metalsColorsDataSet.json")).getAsJsonObject();
-        Object[][] result = new Object[jDataSet.size()][1];
-        int counter = 0;
-        for (String key : jDataSet.keySet()) {
-            result[counter][0] = gson.fromJson(jDataSet.get(key), MetalAndColorsTemplate.class);
-            counter++;
+    @DataProvider
+    public static Object[] metalAndColorsDataProvider() throws FileNotFoundException {
+        Map<String, MetalAndColorsTemplate> result = new HashMap<>();
+        File file = new File(filePath);
+        try (JsonReader jsonReader = new JsonReader(new FileReader(file))) {
+            Type token = new TypeToken<Map<String, MetalAndColorsTemplate>>() {{
+            }}.getType();
+            result = new Gson().fromJson(jsonReader, token);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return result;
+        return result.values().toArray();
 
     }
 }
